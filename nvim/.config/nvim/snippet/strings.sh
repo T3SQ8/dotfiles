d06812a13ad5736a8c3abbf85f62fd48f564277b
T3SQ8 <h9s@tutanota.com>
2021-11-09 22:13:21 +0100

    Added shellscript snippets

---
+++ nvim/.config/nvim/snippet/strings.sh
@@ -0,0 +1,5 @@
+[ "$STRING" ]                # True if string is not null.
+[ -n "$STRING" ]             # True if length of string is nonzero.
+[ -z "$STRING" ]             # True if length of string is zero.
+[ "$STRING1" = "$STRING2" ]  # True if the strings are identical.
+[ "$STRING1" != "$STRING2" ] # True if the strings are not identical.

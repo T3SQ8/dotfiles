9d08f3d8099cf76e5c28e5332a73f7216ae15998
T3SQ8 <h9s@tutanota.com>
2022-01-06 23:27:59 +0100

    Python selenium snippets

--- nvim/.config/nvim/snippet/selenium_init_firefox.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
-
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options)

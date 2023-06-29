d6fc36864a0cc4d96aebd903e747f299d967f0e9
T3SQ8 <h9s@tutanota.com>
2023-06-29 23:20:51 +0200

    Add emacs configs

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,48 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+;(package-initialize)
+;(package-refresh-contents)
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+;;; Ledger
+;(use-package ledger-mode
+;	     :ensure t
+;	     :init
+;
+;	     :config
+;	     (add-to-list 'evil-emacs-state-modes 'ledger-report-mode)
+;	     :mode "\\.ledger\\'")
+
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves/"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+
+(setq org-agenda-files (file-expand-wildcards "~/notes/*.org"))
